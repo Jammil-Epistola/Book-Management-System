9329f3efee3676694a33311fe07d7248a401d3fe
Jammil Epistola <epistolajammil45@gmail.com>
2024-10-30 23:26:11 +0800

    Update App.js

--- book-management-sys/src/App.js
+++ book-management-sys/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import BookDashboard from './components/BookDashboard';
